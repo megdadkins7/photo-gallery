5a64e8182a8e1af834f74c36504fca0f3f80163f
Meghan Adkins <meghandadkins@gmail.com>
2020-08-17 10:28:30 -0400

    inital setup and styles of photo gallery

---
+++ src/data.js
@@ -0,0 +1,70 @@
+//assets
+import breach from "../assets/breach.png";
+import brimstone from "../assets/brimstone.png";
+import cypher from "../assets/cypher.png";
+import jett from "../assets/jett.png";
+import omen from "../assets/omen.png";
+import phoenix from "../assets/phoenix.png";
+import raze from "../assets/raze.png";
+import reyna from "../assets/reyna.png";
+import sage from "../assets/sage.png";
+import sova from "../assets/sova.png";
+import viper from "../assets/viper.png";
+
+export const heroList = [
+  {
+    id: "breach",
+    name: "Breach",
+    pic: breach,
+  },
+  {
+    id: "brimstone",
+    name: "Brimstone",
+    pic: brimstone,
+  },
+  {
+    id: "cypher",
+    name: "Cypher",
+    pic: cypher,
+  },
+  {
+    id: "jett",
+    name: "Jett",
+    pic: jett,
+  },
+  {
+    id: "omen",
+    name: "Omen",
+    pic: omen,
+  },
+  {
+    id: "phoenix",
+    name: "Phoenix",
+    pic: phoenix,
+  },
+  {
+    id: "raze",
+    name: "Raze",
+    pic: raze,
+  },
+  {
+    id: "reyna",
+    name: "Reyna",
+    pic: reyna,
+  },
+  {
+    id: "sage",
+    name: "Sage",
+    pic: sage,
+  },
+  {
+    id: "sova",
+    name: "Sova",
+    pic: sova,
+  },
+  {
+    id: "viper",
+    name: "Viper",
+    pic: viper,
+  },
+];
